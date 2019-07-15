ec38be13cb1fcdaf76786c5fc8a5ac1afd09f2ab
devJake <devjake@outlook.com>
2019-07-15 09:43:20 -0600

    Initial Commit

---
+++ noodle-feed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  noodle-feed
+//
+//  Created by Jake Stanger on 7/15/19.
+//  Copyright © 2019 Jake Stanger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
